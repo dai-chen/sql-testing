0bf0ae26f8b69ca9cd9b155699a9e0caeb4ed3ed
Chen Dai <46505291+dai-chen@users.noreply.github.com>
2019-12-02 11:27:44 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,17 @@
 
 git clone https://github.com/dai-chen/sql.git
-
 cd sql
-
 git checkout correctness-poc
 
 ./gradlew integTestRunner
-
 cp -rf build/reports/tests/integTestRunner/* ..
 
 cd ..
 rm -rf sql
+
+git config user.email "daichen@amazon.com"
+git config user.name "Dai"
+git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+
 git commit -am "Test report"
 git push
